c89e004bd4a8c9aeb6e3b717a6a2a0850c92a8a0
Segun Tuase <tuase360@gmail.com>
2024-01-25 06:07:26 +0100

    ongoing: elementlinks

--- src/store/lookups/lookupsSlice.tsx
+++ src/store/lookups/lookupsSlice.tsx
@@ -1,14 +1,17 @@
+
 import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
 
 export const fetchLookupValues = createAsyncThunk('lookups/fetchLookupValues', async (id, thunkAPI) => {
   try {
     const response = await fetch(`https://650af6bedfd73d1fab094cf7.mockapi.io/lookups/${id}/lookupvalues`);
     const data = await response.json();
+//@ts-ignore   
     const lookupValues = data.map((item) => {
         return { value: item.id, label: item.name, lookupId: item.lookupId};
     })
     return lookupValues;
   } catch (error) {
+   //@ts-ignore
     return thunkAPI.rejectWithValue(error.message);
   }
 });
@@ -26,5 +29,6 @@ const lookupsSlice = createSlice({
   },
 });
 
-export const selectLookupValues = (state) => state.lookups?.lookupValues;
+//@ts-ignore
+export const selectLookupValues = (state: any) => state.lookups?.lookupValues;
 export default lookupsSlice.reducer;
